d0bf7288eaab06f9db53cd94654e7eec87851319
alrowaithy@gmail.com <alrowaithy@gmail.com@8129b613-3fb8-c384-721d-9b758129ca17>
2012-05-01 06:02:03 +0000

    Got rid of useless functions (storeUser, isUserLoggedIn, getUser)

--- trunk/BestFit/src/com/bestfit/client/RpcServices.java
+++ trunk/BestFit/src/com/bestfit/client/RpcServices.java
@@ -9,9 +9,6 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("services")
 public interface RpcServices extends RemoteService {
-	Bridge getUsers(String email) throws IllegalArgumentException;
-	Bridge saveUsers(String email) throws IllegalArgumentException;
-	Bridge logIn(String url) throws IllegalArgumentException;
 	String registerUser(Bridge msg) throws IllegalArgumentException;
 	String getCurrentWeight()throws IllegalArgumentException;
 	boolean isNewUser() throws IllegalArgumentException;
--- trunk/BestFit/src/com/bestfit/client/RpcServicesAsync.java
+++ trunk/BestFit/src/com/bestfit/client/RpcServicesAsync.java
@@ -7,9 +7,6 @@ import com.bestfit.shared.Bridge;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface RpcServicesAsync {
-	void getUsers(String email, AsyncCallback<Bridge> callback) throws IllegalArgumentException;
-	void saveUsers(String email, AsyncCallback<Bridge> callback);
-	void logIn(String url, AsyncCallback<Bridge> callback);
 	void registerUser(Bridge msg, AsyncCallback<String> callback);
 	void getCurrentWeight(AsyncCallback<String> callback);
 	void isNewUser(AsyncCallback<Boolean> callback);
