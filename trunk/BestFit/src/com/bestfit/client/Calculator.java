a4b53cd7a31d362af6f50976c21e71562b49667a
alrowaithy@gmail.com <alrowaithy@gmail.com@8129b613-3fb8-c384-721d-9b758129ca17>
2012-05-04 02:52:51 +0000

    got rid of saveFoodItem and saveExerciseItem, added them to admin.java

--- trunk/BestFit/src/com/bestfit/client/Calculator.java
+++ trunk/BestFit/src/com/bestfit/client/Calculator.java
@@ -208,59 +208,6 @@ public class Calculator implements EntryPoint {
 		WorkoutFlexTable.getFlexCellFormatter().setColSpan(1, 0, 2);
 		WorkoutFlexTable.getFlexCellFormatter().setColSpan(1, 0, 5);
 		
-		// one time run to populate FoodItem persistence
-//		{
-//			// name, cals, fatcals, fatgrams, carbs, protein
-//			FoodItem items[] = new FoodItem[10];
-//			items[0] = new FoodItem("Egg, Whole, Cooked, Medium", 71, 40, 5.15, .48, 5.28);
-//			items[1] = new FoodItem("Egg, Whole, Cooked, Large", 81, 45, 5.86, .55, 6);
-//			items[2] = new FoodItem("Egg, Whole, Cooked, Extra Large", 94, 51, 6.79, .64, 6.96);
-//			items[3] = new FoodItem("Bacon, Medium Slice", 43, 40, 3.34, .11, 2.96);
-//			items[4] = new FoodItem("Pancake, Buttermilk, 5-1/2\" dia", 110, 28, 2.53, 19.25, 2.61);
-//			items[5] = new FoodItem("Waffle, Round, 4\" dia", 121, 36, 3.72, 19.05, 2.85);
-//			items[6] = new FoodItem("Bread, White, Toasted, 1 Slice", 73, 17, 1, 13.6, 2.25);
-//			items[7] = new FoodItem("Bread, Wheat, Toasted, 1 Slice", 76, 16, 1.02, 12.79, 4.07);
-//			items[8] = new FoodItem("Milk, 2%, 1 Cup", 122, 68, 4.83, 11.71, 8.05);
-//			items[9] = new FoodItem("Orange Juice, 1 Cup", 122, 7, .3, 28.73, 1.69);
-//			for (int i = 0; i < items.length; i++) {
-//				saveFoodItem(items[i]);
-//				if (saveFoodItem(items[i]))
-//					System.out.println("Saved item " + i + "(" + items[i].getName() + ") successfully.");
-//				else
-//					System.err.println("Could not save item " + i + "(" + items[i].getName() + ").");
-//				NewFoodItem.addItem(items[i].getName());
-//				foods.add(items[i]);
-//			}
-//		}
-		
-		
-		
-		// one time run to populate ExerciseItem persistence
-		{
-			// name, burnrate
-//			ExerciseItem items[] = new ExerciseItem[13];
-//			items[0] = new ExerciseItem("Basketball (leisurely)", 130);
-//			items[1] = new ExerciseItem("Bicycling (10 mph)", 125);
-//			items[2] = new ExerciseItem("Bicycling (13 mph)", 200);
-//			items[3] = new ExerciseItem("Ping pong", 90);
-//			items[4] = new ExerciseItem("Running (8 mph)", 305);
-//			items[5] = new ExerciseItem("Running (9 mph)", 330);
-//			items[6] = new ExerciseItem("Running (10 mph)", 350);
-//			items[7] = new ExerciseItem("Soccer", 195);
-//			items[8] = new ExerciseItem("Swimming", 120);
-//			items[9] = new ExerciseItem("Tennis", 160);
-//			items[10] = new ExerciseItem("Walking (3 mph)", 80);
-//			items[11] = new ExerciseItem("Walking (4 mph)", 100);
-//			items[12] = new ExerciseItem("Weight training", 190);
-//			for (int i = 0; i < items.length; i++) {
-//				saveExerciseItem(items[i]);
-//				saveExerciseItem(items[i]);
-//				NewExerciseItem.addItem(items[i].getName());
-//				exercises.add(items[i]);
-//			}
-		}
-		
-		
 		newMeal = new Meal(email);
 		newWorkout = new Workout(email);
 		startAsynchronous();
@@ -385,28 +332,5 @@ public class Calculator implements EntryPoint {
 		});
 	}
 	
-	
-	
-	
-	
-	
-//	private void saveFoodItem(FoodItem item) {
-//		Bridge msg = new Bridge();
-//		msg.foodItem = item;
-//		rpc.saveFoodItem(msg, new AsyncCallback<Boolean>() {
-//			public void onFailure(Throwable caught) {}
-//
-//			public void onSuccess(Boolean result) {}
-//		});
-//	}	
-//		
-//	private void saveExerciseItem(ExerciseItem item) {
-//		Bridge msg = new Bridge();
-//		msg.exercise = item;
-//		rpc.saveExerciseItem(msg, new AsyncCallback<Boolean>() {
-//			public void onFailure(Throwable caught) {}
-//
-//			public void onSuccess(Boolean result) {}
-//		});
-//	}
+
 }
